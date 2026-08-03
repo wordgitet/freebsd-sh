d0cc8e3688855f300af70fb984b74848e4607ff8
wordgitet <wordatet@linuxmail.org>
2026-08-03 19:32:33 +0300

    trap: restore defaults after trap - and async traps
    Clear inherited hard-ignore state when trap - resets a handler so
    job-control signals regain default disposition in non-interactive
    scripts. Allow explicit trap commands to override S_HARD_IGN for
    SIGINT and SIGQUIT in async lists without monitor mode.
    
    Areas:
     trap: trap - clears siginherited and tty stop defaults
     trap: drop iflag gate on hard-ignore reset for trapcmd
    
    Signed-off-by: wordgitet <wordatet@linuxmail.org>

--- src/trap.c
+++ src/trap.c
@@ -282,8 +282,11 @@ trapcmd(int argc __unused, char **argv)
 		trap[signo] = action;
 		if (signo != 0) {
 			trap_force_signo = signo;
-			if (force_default && is_tty_stopsig(signo) && iflag)
-				trap_default_signo = signo;
+			if (force_default) {
+				siginherited[signo] = 0;
+				if (is_tty_stopsig(signo))
+					trap_default_signo = signo;
+			}
 			setsignal(signo);
 			trap_default_signo = 0;
 			trap_force_signo = 0;
@@ -411,7 +414,7 @@ setsignal(int signo)
 	if (trap_default_signo == signo &&
 	    (*t == S_HARD_IGN || *t == S_IGN))
 		*t = S_RESET;
-	if (*t == S_HARD_IGN && trap_force_signo == signo && iflag &&
+	if (*t == S_HARD_IGN && trap_force_signo == signo &&
 	    !tty_stopsig_preserve_ign(signo))
 		*t = S_RESET;
 	if (*t == S_HARD_IGN || *t == action)
