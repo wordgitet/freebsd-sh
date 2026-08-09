96ebb665caf1ad519df2fa31d2dbd98754666dd7
wordgitet <wordatet@linuxmail.org>
2026-08-10 00:44:38 +0300

    Revert "shell: guard nested async signal forks"
    This reverts commit 8aa21fd55a49b12cc6238d5dbb71befffd7bd1f5.

--- src/parser.c
+++ src/parser.c
@@ -2345,7 +2345,6 @@ getprompt(void *unused __unused)
 	const char *fmt;
 	const char *home;
 	const char *pwd;
-	const char *expanded;
 	size_t homelen;
 	int i, trim;
 	static char internal_error[] = "??";
@@ -2366,11 +2365,6 @@ getprompt(void *unused __unused)
 	default:
 		return internal_error;
 	}
-
-	/* POSIX prompt strings undergo the normal parameter expansion. */
-	expanded = expandstr(fmt);
-	if (expanded != NULL)
-		fmt = expanded;
 
 #if !NEOASH_EXTENSIONS
 	strlcpy(ps, fmt, sizeof(ps));
